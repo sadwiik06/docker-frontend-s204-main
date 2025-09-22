e2b63225ca9f8c600022f1af5634b4f77ca7790e
KATAM SAI SADWIIK <2300032291@kluniversity.in>
2025-09-22 10:24:54 +0530

    Update cartService.js

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://ec2-3-26-12-51.ap-southeast-2.compute.amazonaws.com:8081/api/cart";
+const API_URL = "http://16.171.160.180:8081/api/cart";
 
-const API_BASE = "http://ec2-3-26-12-51.ap-southeast-2.compute.amazonaws.com:8081/api/cart";
+const API_BASE = "http://16.171.160.180:8081/api/cart";
 
 export const addToCart = async (userId, productId) => {
   return await axios.post(`${API_BASE}/add`, {
