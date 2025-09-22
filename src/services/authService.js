851cc15cd5998bf10116886783c3f189dafd530e
KATAM SAI SADWIIK <2300032291@kluniversity.in>
2025-09-22 10:24:38 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://ec2-54-252-188-16.ap-southeast-2.compute.amazonaws.com:8081/auth";
+const API_URL = "http://16.171.160.180:8081/auth";
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
